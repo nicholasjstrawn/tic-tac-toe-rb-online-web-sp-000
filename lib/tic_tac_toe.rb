2390f06709d12a03eff0d082912ff9cc8532d304
Nicholas Strawn <nicholasjstrawn@gmail.com>
2021-03-22 22:41:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,8 +61,8 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-    move(board, index, current_player(board))
-    display_board
+    player_move(board, index, current_player(board))
+    display_board(board)
   else
     turn(board)
   end
