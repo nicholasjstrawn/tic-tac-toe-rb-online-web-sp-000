2454c1865d1cb124aa26515683ce985f2d3f3b58
Nicholas Strawn <nicholasjstrawn@gmail.com>
2021-03-22 23:11:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -94,9 +94,9 @@ def full?(board)
 end
 
 def draw?(board)
-  if !won?(board) && full?(board)
+  if won?(board) == false && full?(board) == true
     return true
-  elsif !full?(board) && !won?(board)
+  elsif full?(board) == false && won?(board) == false
     return false
   else won?(board)
     return false
